7b35f675bd0ef5d719078450c484a886e2c34482
Nikita Barsukov <n.barsukov@tinkoff.ru>
2020-10-22 18:46:46 +0300

    Updates

---
+++ precache-manifest.ca60bcd1cb6c606120c961c17a72a157.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbbce9b2ccc73298acc43593631f389",
+    "url": "/preza-demo/index.html"
+  },
+  {
+    "revision": "e9d1aea339a503a9d7e0",
+    "url": "/preza-demo/static/css/2.89d3079d.chunk.css"
+  },
+  {
+    "revision": "e9d1aea339a503a9d7e0",
+    "url": "/preza-demo/static/js/2.009f6e67.chunk.js"
+  },
+  {
+    "revision": "7dc041ffd3b4f66797da40145a683b9c",
+    "url": "/preza-demo/static/js/2.009f6e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b18528775b9e605d8e6",
+    "url": "/preza-demo/static/js/main.6c6baf49.chunk.js"
+  },
+  {
+    "revision": "c27b56e85925a3139359",
+    "url": "/preza-demo/static/js/runtime-main.b9d5cf37.js"
+  },
+  {
+    "revision": "72217712eb8d28872e7069322f3fda23",
+    "url": "/preza-demo/static/media/source-sans-pro-italic.72217712.eot"
+  },
+  {
+    "revision": "8256cfd7e4017a7690814879409212cd",
+    "url": "/preza-demo/static/media/source-sans-pro-italic.8256cfd7.ttf"
+  },
+  {
+    "revision": "e74f0128884561828ce8c9cf5c284ab8",
+    "url": "/preza-demo/static/media/source-sans-pro-italic.e74f0128.woff"
+  },
+  {
+    "revision": "1d71438462d532b62b05cdd7e6d7197d",
+    "url": "/preza-demo/static/media/source-sans-pro-regular.1d714384.eot"
+  },
+  {
+    "revision": "2da39ecf9246383937da11b44b7bd9b4",
+    "url": "/preza-demo/static/media/source-sans-pro-regular.2da39ecf.ttf"
+  },
+  {
+    "revision": "e7acc589bb558fe58936a853f570193c",
+    "url": "/preza-demo/static/media/source-sans-pro-regular.e7acc589.woff"
+  },
+  {
+    "revision": "0f3da1edf1b5c6a94a6ad948a7664451",
+    "url": "/preza-demo/static/media/source-sans-pro-semibold.0f3da1ed.eot"
+  },
+  {
+    "revision": "1cb8e94f1185f1131a0c895165998f2b",
+    "url": "/preza-demo/static/media/source-sans-pro-semibold.1cb8e94f.woff"
+  },
+  {
+    "revision": "f3565095e6c9158140444970f5a2c5ed",
+    "url": "/preza-demo/static/media/source-sans-pro-semibold.f3565095.ttf"
+  },
+  {
+    "revision": "58153ac7194e141d1e73ea88c6b63861",
+    "url": "/preza-demo/static/media/source-sans-pro-semibolditalic.58153ac7.eot"
+  },
+  {
+    "revision": "6b058fc2634b01d837c3432316c3141f",
+    "url": "/preza-demo/static/media/source-sans-pro-semibolditalic.6b058fc2.woff"
+  },
+  {
+    "revision": "c7e698a4d0956f4a939f42a05685bbf5",
+    "url": "/preza-demo/static/media/source-sans-pro-semibolditalic.c7e698a4.ttf"
+  }
+]);
